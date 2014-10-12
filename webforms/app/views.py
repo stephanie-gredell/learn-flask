b9dbc15f431b516aa3f8a06afd9955cd3b48b4f4
Stephanie Gredell <stephanie@localhost.localdomain>
2014-10-11 18:20:30 -0700

    adding lesson in web forms

---
+++ webforms/app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, flash, redirect
+from app import app
+from forms import LoginForm
+@app.route('/')
+@app.route('/index')
+def index():
+  user = {'nickname': 'Stephanie'} #fake user
+  posts = [
+      {
+        'author': {'nickname': 'John'},
+        'body': 'Beautiful day in Portland'
+        },
+      {
+        'author': {'nickname': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+        }
+      ]
+  return render_template('index.html', user=user, posts=posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for OpenID="%s", remember_me=%s' %
+        (form.openid.data, str(form.remember_me.data)))
+    return redirect('/index')
+  return render_template('login.html',
+      title='Sign In',
+      form=form,
+      providers=app.config['OPENID_PROVIDERS'])
