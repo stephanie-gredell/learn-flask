a0cbd1849bcd9593921b786dce4ac54a337e5eeb
Stephanie Gredell <stephanie@localhost.localdomain>
2014-10-16 19:07:45 -0700

    Adding a database

---
+++ database/config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+WTF_CSRF_ENABLED = True #activates cross-site request forgery prevention
+SECRET_KEY = 'something-goes-here' #needed when csrf is enabled, token that is used to validate form
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/08/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
